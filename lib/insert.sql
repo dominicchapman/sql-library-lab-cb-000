7cbe16e210d3dbfc7673dccb4ecbf154992fdf0d
Dominic Chapman <dominicchapman@me.com>
2017-10-23 23:30:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,12 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES
 (1, "First series", 3, 1),
 (2, "Second series", 2, 2);
+
+INSERT INTO books (id, title, year, series_id) VALUES
+(1, "Harry Potter and the Philosopher's Stone", 2001, 1),
+(2, "Harry Potter and the Chamber of Secrets", 2002, 1),
+(3, "Harry Potter and the Prisoner of Azkaban", 2004, 1),
+(4, "Harry Potter and the Goblet of Fire", 2005, 1),
+(5, "Harry Potter and the Order of the Phoenix", 2007, 1),
+(6, "Harry Potter and the Half-Blood Prince", 2009, 1),
+;
