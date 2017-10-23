5e3a1e276347a7b6069514dd67ca81bf4e37e63c
Dominic Chapman <dominicchapman@me.com>
2017-10-23 23:34:25 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,3 +9,13 @@ INSERT INTO books (id, title, year, series_id) VALUES
 (4, "Harry Potter and the Goblet of Fire", 2005, 1),
 (5, "Harry Potter and the Order of the Phoenix", 2007, 1),
 (6, "Harry Potter and the Half-Blood Prince", 2009, 1);
+
+INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES
+(1, "Harry", "Wizard", "Gotcha, Volde", 1, 1),
+(2, "Ron", "Wizard", "Woo, Hermoine", 1, 1),
+(3, "Hermoine", "Wizard", "I love learning", 1, 1),
+(4, "Voldemort", "Wizard", "Grr", 1, 1),
+(5, "Dumbeldore", "Wizard", "Love is life", 1, 1),
+(6, "Snape", "Wizard", "Snape, Snape, Severus Snape", 1, 1),
+(7, "Hagrid", "Wizard", "Spiders are friends", 1, 1),
+(8, "Malfoy", "Wizard", "Ugh, muggles", 1, 1);
